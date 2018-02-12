729f5286a15abef49cc46351a7b90989023b6290
robond <megnath@ualberta.ca>
2018-02-12 15:27:17 -0800

    Exercise 3 almost done

--- Exercise-3/sensor_stick/src/sensor_stick/features.py
+++ Exercise-3/sensor_stick/src/sensor_stick/features.py
@@ -34,12 +34,16 @@ def compute_color_histograms(cloud, using_hsv=False):
         channel_3_vals.append(color[2])
     
     # TODO: Compute histograms
+    r_hist = np.histogram(channel_1_vals, bins = 32, range=(0,256))
+    g_hist = np.histogram(channel_2_vals, bins = 32, range=(0,256))
+    b_hist = np.histogram(channel_3_vals, bins = 32, range=(0,256))
 
     # TODO: Concatenate and normalize the histograms
+    hist_features = np.concatenate((r_hist[0], g_hist[0], b_hist[0])).astype(np.float64)
+    normed_features = hist_features / np.sum(hist_features)
 
     # Generate random features for demo mode.  
-    # Replace normed_features with your feature vector
-    normed_features = np.random.random(96) 
+    # Replace normed_features with your feature vector 
     return normed_features 
 
 
@@ -56,8 +60,13 @@ def compute_normal_histograms(normal_cloud):
         norm_z_vals.append(norm_component[2])
 
     # TODO: Compute histograms of normal values (just like with color)
+    x_hist = np.histogram(norm_x_vals, bins = 32, range =(0,256))
+    y_hist = np.histogram(norm_y_vals, bins = 32, range =(0,256))
+    z_hist = np.histogram(norm_z_vals, bins = 32, range =(0,256))
 
     # TODO: Concatenate and normalize the histograms
+    hist_features = np.concatenate((x_hist[0], y_hist[0], z_hist[0])).astype(np.float64)
+    normed_features = hist_features / np.sum(hist_features)
 
     # Generate random features for demo mode.  
     # Replace normed_features with your feature vector
